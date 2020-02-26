981742c948049ccb6cdff96cbe0a0ff644e2e052
Sergei Shneider <shneider.s@gmail.com>
2020-02-26 00:31:25 -0500

    started working on charts. content is there needs styling now

--- client/components/stock-display-single.js
+++ client/components/stock-display-single.js
@@ -1,15 +1,45 @@
 import React from "react";
 import "./stock-display-single.css";
 import {Col, Card} from 'react-bootstrap'
+import { Chart } from "react-google-charts";
 /**
  * COMPONENT
  */
 export default function SingleStock(props){
-    console.log(props)
+    //Start Graph Options
+    const graphOptions = {
+        curveType: "function",
+        legend: { position: "none" },
+        hAxis: { textPosition: 'none' },
+        chartArea: {'width': '75%', 'height': '70%'}
+      }
+    const graphData =[
+        ["Date", "Price"],
+    ]
+    props.stock.chart.forEach(entry => {
+        graphData.push([entry.date, entry.open])
+    })
+    //End Graph Options
+    const {symbol, change, companyName, marketCap, changePercent} = props.stock.quote
  return(
      <Col>
-     <Card>
-    <Card.Title>{props.stock.quote.symbol}</Card.Title>
+    
+     <Card className="singleStockCard">
+         
+    <Card.Body>
+    <Card.Title>{symbol}<span>{companyName}</span></Card.Title>
+    
+    {change}
+    {changePercent}
+    asds
+    {marketCap}
+    
+    <Chart
+          chartType="LineChart"
+          data={graphData}
+          options={graphOptions}
+        />
+        </Card.Body>
     </Card>
     </Col>
     )   
